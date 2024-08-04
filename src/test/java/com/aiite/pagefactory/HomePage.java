5d202385cc3f88f17d5739e2d6bf5e58f261a62f
Azharudeen Jaferali <ajaferali@enhops.com>
2024-08-04 11:02:36 +0530

    added new class files

--- src/test/java/com/aiite/pagefactory/HomePage.java
+++ src/test/java/com/aiite/pagefactory/HomePage.java
@@ -4,5 +4,6 @@ public class HomePage {
 	
 	public String searchBox = "twotabsearchtextbox"; //ID
 	public String searchBtn = "//input[@value='Go']"; //xpath
+	public String menuBtn = "//input[@value='Go']"; //xpath
 
 }
---
+++ src/test/java/com/aiite/pagefactory/SummaryPage.java
@@ -0,0 +1,7 @@
+package com.aiite.pagefactory;
+
+public class SummaryPage {
+	public String userName = "email"; //ID
+	public String passWord = "pass"; //ID
+	public String loginBtn = "//button[@name='login']"; //Xpath
+}
